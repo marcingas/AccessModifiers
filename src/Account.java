e8d580cad45be00b4eccc25d250a34a0175c773f
Marcin <gasiorek.mail@gmail.com>
2023-02-09 10:44:38 +0100

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Account {
+    public String accountName;
+    public int balance = 0;
+    public ArrayList<Integer> transactions;
+
+    public Account(String accountName) {
+        this.accountName = accountName;
+        this.transactions = new ArrayList<Integer>();
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
